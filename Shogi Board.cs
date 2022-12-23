5e31caba5f4e8701d1f403e94f0d0808012a4f36
Doom Art <Ouginak121@gmail.com>
2022-12-23 04:42:52 -0500

    started shogi

---
+++ Shogi Board.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect_four
+{
+    internal class Shogi_Board
+    {
+        private Texture2D _pawn;
+        private Texture2D _lance;
+        private Texture2D _knight;
+        private Texture2D _silverGeneral;
+        private Texture2D _goldGeneral;
+        
+    }
+}
